b2ba0506a5ca65f08357c8c5da7dbfc3dafbbf35
Amazing <amazing@AmazingdeMacBook-Air.local>
2015-10-06 21:12:46 +0800

    [add] findermaincell

---
+++ HappyTravelShow/HappyTravelShow/Classes/General/Views/FinderMainCell/FinderMainCell.m
@@ -0,0 +1,23 @@
+//
+//  FinderMainCell.m
+//  HappyTravelShow
+//
+//  Created by Amazing on 15/10/6.
+//  Copyright (c) 2015年 com.liuwei. All rights reserved.
+//
+
+#import "FinderMainCell.h"
+
+@implementation FinderMainCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
