c1f28f00f614dfb0e23aa8870f10c55f273e9a7e
Amazing <amazing@AmazingdeMacBook-Air.local>
2015-10-06 21:25:10 +0800

    [add]----

---
+++ HappyTravelShow/HappyTravelShow/Classes/Macro/FinderURL.h
@@ -0,0 +1,16 @@
+//
+//  FinderURL.h
+//  HappyTravelShow
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright (c) 2015年 com.liuwei. All rights reserved.
+//
+
+#ifndef HappyTravelShow_FinderURL_h
+#define HappyTravelShow_FinderURL_h
+
+//图片链接拼接
+#define  kURL(url) [NSString stringWithFormat:@"http://cdn1.jinxidao.com/%@",url]
+
+
+#endif
