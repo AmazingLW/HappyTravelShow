e4953e617d14e33f8151907fc055128c6e3647b8
wanghewei <wahewe@163.com>
2015-10-07 16:50:28 +0800

    [add] aroudn 页面

--- HappyTravelShow/HappyTravelShow/Classes/Scenes/ViewController/AroundVC/AroundVC.m
+++ HappyTravelShow/HappyTravelShow/Classes/Scenes/ViewController/AroundVC/AroundVC.m
@@ -7,8 +7,16 @@
 //
 
 #import "AroundVC.h"
+#import "XIOptionSelectorView.h"
+#import "XIOptionView.h"
+#import "XIOtherOptionsView.h"
+#import "XIColorHelper.h"
 
-@interface AroundVC ()
+@interface AroundVC ()<XIDropdownlistViewProtocol,UITableViewDelegate,UITableViewDataSource>
+{
+     XIOptionSelectorView *ddltView;
+}
+
 
 @end
 
@@ -27,7 +35,127 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-   // self.view.backgroundColor = [UIColor greenColor];
+    
+    
+    
+ //tableview 创建
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 40, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    
+    [self.view addSubview:tableView];
+    
+    [self setupDropdownList];
+}
+
+
+#pragma mark ---tableview 代理事件
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    
+    return 100;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *const reuse = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuse];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuse];
+    }
+    cell.textLabel.text = @"sdgswh";
+    return cell;
+}
+
+#pragma mark --顶部4个按钮的创建
+- (void)setupDropdownList
+{
+    ddltView = [[XIOptionSelectorView alloc] initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, 40)];
+    ddltView.parentView = self.view;
+    ddltView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [self.view addSubview:ddltView];
+    
+    __weak typeof(self) weakSelf = self;
+    [ddltView setViewAtIndex:^UIView<XIDropdownlistViewProtocol> *(NSInteger index, XIOptionSelectorView*opView) {
+        
+        XIOptionSelectorView *strongOpSelectorRef = opView;
+        UIView<XIDropdownlistViewProtocol> *aView;
+        CGFloat py = strongOpSelectorRef.frame.size.height+strongOpSelectorRef.frame.origin.y;
+        CGFloat dpW = weakSelf.view.frame.size.width;
+        
+        if(index<2){
+            
+            aView = [[XIOptionView alloc] initWithFrame:CGRectMake(0, py, dpW, 240)];
+            aView.backgroundColor = [UIColor whiteColor];
+            aView.delegate = weakSelf;
+            aView.viewIndex = index;
+            //第二个数组的个数是根据数据请求来得
+            NSArray *tmpArray = (index==0)? @[@"默认排序", @"价格由低至高", @"价格由高至低",@"销量优先",@"新品优先",@"离我最近"]:@[@"全部",@"北京"];
+            [aView setFetchDataSource:^NSArray *{
+                return tmpArray;
+            }];
+        }else if (index == 3){
+            aView = [[XIOptionView alloc] initWithFrame:CGRectMake(0, py, dpW, 240)];
+            aView.backgroundColor = [UIColor whiteColor];
+            aView.delegate = weakSelf;
+            aView.viewIndex = index;
+            //第二个数组的个数是根据数据请求来得
+            NSArray *tmpArray = @[@"全部",@"门票",@"仅酒店",@"酒店套餐"];
+            [aView setFetchDataSource:^NSArray *{
+                return tmpArray;
+            }];
+            
+        }else{
+            aView = [[XIOtherOptionsView alloc] initWithFrame:CGRectMake(0, py, dpW, 240)];
+            aView.backgroundColor = [UIColor whiteColor];
+            aView.delegate = weakSelf;
+            aView.viewIndex = index;
+            
+            [aView setFetchDataSource:^NSArray *{
+                //根据数据请求的数组
+                return @[@"全部", @"bbb",@"cvvv"];
+            }];
+        }
+        //aView.hidden = YES;
+        [weakSelf.view addSubview:aView];
+        return aView;
+    }];
+}
+
+#pragma XIDropdownlistViewProtocol method  点击方法
+
+- (void)didSelectItemAtIndex:(NSInteger)index inSegment:(NSInteger)segment
+{
+    NSArray *tmpArry;
+    if(segment==0){
+        tmpArry = @[@"默认排序", @"价格变高", @"价格变低",@"销量优先",@"新品优先",@"离我最近"];
+        [ddltView setTitle:tmpArry[index] forItem:segment];
+    }
+    else if(segment==1){
+        //根据请求数组
+        tmpArry = @[@"全部", @"北京"];
+        [ddltView setTitle:tmpArry[index] forItem:segment];
+    }
+    else if(segment == 2){
+        //请求数组
+        //NSLog(@"%s, %ld", __FUNCTION__, (long)index);
+        tmpArry = @[@"全部", @"bbb",@"cvvv"];
+        [ddltView setTitle:tmpArry[index] forItem:segment];
+        
+    }else{
+        
+        tmpArry = @[@"全部",@"门票",@"仅酒店",@"酒店套餐"];
+        //测试点击事件
+        //        if (index == 1) {
+        //            ViewController *viewc = [ViewController new];
+        //            [self.navigationController pushViewController:viewc animated:YES];
+        //        }else{
+        //
+        //        }
+        
+    }
     
     
 }
