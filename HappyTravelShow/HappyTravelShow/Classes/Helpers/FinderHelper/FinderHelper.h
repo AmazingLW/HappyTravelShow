4cd0b8b37a0bd01d97fe74d0a501063d1ace525f
carrot <13707089317@163.com>
2015-10-07 21:18:30 +0800

    [update] 更新发现页面

--- HappyTravelShow/HappyTravelShow/Classes/Helpers/FinderHelper/FinderHelper.h
+++ HappyTravelShow/HappyTravelShow/Classes/Helpers/FinderHelper/FinderHelper.h
@@ -10,4 +10,17 @@
 
 @interface FinderHelper : NSObject
 
+//单例方法
++ (FinderHelper *)sharedHelper;
+
+//发现主页面请求数据
+- (void)getDataWithCityCode:(NSString *)cityCode pageIndex:(NSInteger )pageIndex Finish:(void(^)())result;
+
+//外界可用到的发现主页面数据的数组
+@property(nonatomic,strong) NSArray  *dataArray;
+//外界可用到的发现各种景点的数组
+@property(nonatomic,strong) NSArray  *kindArray;
+
+
+
 @end
