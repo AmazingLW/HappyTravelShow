5724014c5157325b7030dca16dd7e5d3ca01fb61
AmazingLW <liuwei_0415@126.com>
2015-10-08 09:05:46 +0800

    [add] commence

---
+++ HappyTravelShow/HappyTravelShow/Classes/General/Views/CommonCells/CommonCells.h
@@ -0,0 +1,13 @@
+//
+//  CommonCells.h
+//  HappyTravelShow
+//
+//  Created by Amazing on 15/10/8.
+//  Copyright (c) 2015年 com.liuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonCells : UITableViewCell
+
+@end
---
+++ HappyTravelShow/HappyTravelShow/Classes/General/Views/CommonCells/CommonCells.m
@@ -0,0 +1,23 @@
+//
+//  CommonCells.m
+//  HappyTravelShow
+//
+//  Created by Amazing on 15/10/8.
+//  Copyright (c) 2015年 com.liuwei. All rights reserved.
+//
+
+#import "CommonCells.h"
+
+@implementation CommonCells
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
