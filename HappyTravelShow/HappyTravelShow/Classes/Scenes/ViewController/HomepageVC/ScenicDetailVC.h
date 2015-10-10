4ecf2cc5b5a272cca95e88a68679a86126fe8b10
AmazingLW <liuwei_0415@126.com>
2015-10-10 19:04:56 +0800

    [add] 增加热门景区跳转详情页

---
+++ HappyTravelShow/HappyTravelShow/Classes/Scenes/ViewController/HomepageVC/ScenicDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  ScenicDetailVC.h
+//  HappyTravelShow
+//
+//  Created by Amazing on 15/10/10.
+//  Copyright (c) 2015年 com.liuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScenicDetailVC : UIViewController
+
+@end
