c80c11c33fa00fcf5e6cf20c687c388b91c20ea8
Mayvv <1585414030@qq.com>
2015-10-16 16:40:57 +0800

    [add] button

--- HappyTravelShow/HappyTravelShow/Classes/General/Views/HomepageCell/HotScenicCell.m
+++ HappyTravelShow/HappyTravelShow/Classes/General/Views/HomepageCell/HotScenicCell.m
@@ -58,98 +58,98 @@
     _b1.frame = CGRectMake(0,0,kButtonWidth, 30);
     _b1.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b1 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b1 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b1 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b2=[UIButton buttonWithType:UIButtonTypeCustom];
     _b2.frame = CGRectMake(kButtonWidth,0,kButtonWidth, 30);
     _b2.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b2 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b2 setTitle:@"古井水镇" forState:UIControlStateNormal];
+   // [_b2 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b3=[UIButton buttonWithType:UIButtonTypeCustom];
     _b3.frame = CGRectMake(kWidth/2,0,kButtonWidth, 30);
     _b3.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b3 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b3 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b3 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b4=[UIButton buttonWithType:UIButtonTypeCustom];
     _b4.frame = CGRectMake(kButtonWidth*3,0,kButtonWidth, 30);
     _b4.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b4 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b4 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b4 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b5=[UIButton buttonWithType:UIButtonTypeCustom];
     _b5.frame = CGRectMake(0,30,kButtonWidth, 30);
     _b5.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b5 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b5 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b5 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b6=[UIButton buttonWithType:UIButtonTypeCustom];
     _b6.frame = CGRectMake(kButtonWidth,30,kButtonWidth, 30);
     _b6.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b6 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b6 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b6 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
    _b7=[UIButton buttonWithType:UIButtonTypeCustom];
     _b7.frame = CGRectMake(kWidth/2,30,kButtonWidth, 30);
     _b7.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b7 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b7 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b7 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b8=[UIButton buttonWithType:UIButtonTypeCustom];
     _b8.frame = CGRectMake(kButtonWidth*3,30,kButtonWidth, 30);
     _b8.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b8 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b8 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b8 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
    _b9=[UIButton buttonWithType:UIButtonTypeCustom];
     _b9.frame = CGRectMake(kWidth,0,kButtonWidth, 30);
     _b9.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b9 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b9 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b9 setTitle:@"古井水镇" forState:UIControlStateNormal];
     [_b9 setTitle:@"" forState:UIControlStateHighlighted];
     
     _b10=[UIButton buttonWithType:UIButtonTypeCustom];
     _b10.frame = CGRectMake(kButtonWidth+kWidth,0,kButtonWidth, 30);
     _b10.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b10 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b10 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b10 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b11=[UIButton buttonWithType:UIButtonTypeCustom];
     _b11.frame = CGRectMake(kWidth/2+kWidth,0,kButtonWidth, 30);
     _b11.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b11 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b11 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b11 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b12=[UIButton buttonWithType:UIButtonTypeCustom];
     _b12.frame = CGRectMake(kButtonWidth*3+kWidth,0,kButtonWidth, 30);
     _b12.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b12 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b12 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b12 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b13=[UIButton buttonWithType:UIButtonTypeCustom];
     _b13.frame = CGRectMake(kWidth,30,kButtonWidth, 30);
     _b13.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b13 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b13 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b13 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b14=[UIButton buttonWithType:UIButtonTypeCustom];
     _b14.frame = CGRectMake(kButtonWidth+kWidth,30,kButtonWidth, 30);
     _b14.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b14 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b14 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b14 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b15=[UIButton buttonWithType:UIButtonTypeCustom];
     _b15.frame = CGRectMake(kWidth/2+kWidth,30,kButtonWidth, 30);
     _b15.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b15 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b15 setTitle:@"古井水镇" forState:UIControlStateNormal];
+    //[_b15 setTitle:@"古井水镇" forState:UIControlStateNormal];
     
     _b16=[UIButton buttonWithType:UIButtonTypeCustom];
     _b16.frame = CGRectMake(kButtonWidth*3+kWidth,30,kButtonWidth, 30);
     _b16.titleLabel.font = [UIFont systemFontOfSize:15];
     [_b16 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [_b16 setTitle:@"古井" forState:UIControlStateNormal];
+    //[_b16 setTitle:@"古井" forState:UIControlStateNormal];
     
 
     [_b9 addTarget:self action:@selector(jumpWithDetailsB9) forControlEvents:UIControlEventTouchUpInside];
@@ -175,37 +175,37 @@
 //    cell.layer.borderColor = [UIColor lightGrayColor].CGColor;
 
     [_b1.layer setBorderWidth:0.3f];
-    [_b1.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b1.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b2.layer setBorderWidth:0.3f];
-    [_b2.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b2.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b3.layer setBorderWidth:0.3f];
-    [_b3.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b3.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b4.layer setBorderWidth:0.3f];
-    [_b4.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b4.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b5.layer setBorderWidth:0.3f];
-    [_b5.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b5.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b6.layer setBorderWidth:0.3f];
-    [_b6.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b6.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b7.layer setBorderWidth:0.3f];
-    [_b7.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b7.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b8.layer setBorderWidth:0.3f];
-    [_b8.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b8.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b9.layer setBorderWidth:0.3f];
-    [_b9.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b9.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b11.layer setBorderWidth:0.3f];
-    [_b11.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b11.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b12.layer setBorderWidth:0.3f];
-    [_b12.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b12.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b13.layer setBorderWidth:0.3f];
-    [_b13.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b13.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b14.layer setBorderWidth:0.3f];
-    [_b14.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b14.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b15.layer setBorderWidth:0.3f];
-    [_b15.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b15.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b16.layer setBorderWidth:0.3f];
-    [_b16.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b16.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     [_b10.layer setBorderWidth:0.3f];
-    [_b10.layer setBorderColor:[[UIColor colorWithWhite:0.840 alpha:1.000] CGColor]];
+    [_b10.layer setBorderColor:[[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1] CGColor]];
     
     
     self.OrangView = [[UIView alloc]initWithFrame:CGRectMake(kWidth/8, 39,kWidth/4, 2)];
